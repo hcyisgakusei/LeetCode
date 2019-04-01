b07263fbb6488f8fa74b962df6984d652e0ef7ed
chaya.huang <chaya.huang@klook.com>
2019-04-02 01:18:08 +0800

    6

---
+++ src/6/index.js
@@ -0,0 +1,54 @@
+/*
+6.ZigZag Conversion
+Question:
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+
+Example:
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+const convert = function (s, numRows) {
+    if (numRows === 1) {
+        return s;
+    }
+    const resultArr = Array.from({length: numRows}, () => '');
+    const range = numRows * 2 - 2;
+    for (let i = 0; i < s.length; i++) {
+        if (i % range < numRows) {
+            resultArr[i % range] = resultArr[i % range] + s[i];
+        } else {
+            resultArr[range - i % range] = resultArr[range - i % range] + s[i];
+        }
+    }
+    return resultArr.reduce((acc, cur) => {
+        return acc += cur;
+    }, '');
+};
+
+
+
