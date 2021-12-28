4ff3b3d05344ccfceadd4eb578ff6d30ec3228cc
chaya.huang <chaya.huang@klook.com>
2021-12-28 19:23:55 +0800

    feat: 数组&双指针

--- src/数组&双指针/4_寻找两个正序数组的中位数.js
+++ src/数组&双指针/4_寻找两个正序数组的中位数.js
@@ -34,27 +34,37 @@
  * @return {number}
  */
 const findMedianSortedArrays = function (nums1, nums2) {
-  const mid = (nums1.length + nums2.length) / 2;
-  let index1 = 0, index2 = 0;
-  const result = [];
-  while (result.length <= mid) {
-
-    if ((nums1[index1] < nums2[index2] || 0) || index2 >= nums2.length) {
-      result.push(nums1[index1]);
-      index1++;
+  // 找出分割线，使得左边的所有元素 <= 右边的所有元素
+  // 分割线位置：m + n 为偶数 --> left size = right size； m + n 为奇数 -->  left size - 1 = right size(左边多一个元素)
+  // 分割线条件：由于两个都是有序数组，在同一个数组内，左边的元素肯定小于等于左边的元素。在不同的数组之间，需要满足交叉满足左边小于等于右边！
+  if (nums2.length > nums1.length) {
+    [nums1, nums2] = [nums2, nums1];
+  }
+  const nums1Len = nums1.length, nums2Len = nums2.length;
+  const totalLeft = Math.ceil((nums1Len + nums2Len) / 2);
+  let left = 0, right = nums1Len;
+  while (left < right) {
+    let i = left + Math.ceil((right - left) / 2), j = totalLeft - i;
+    if (nums1[i - 1] > nums2[j]) {
+      right = i - 1;
     } else {
-      result.push(nums2[index2]);
-      index2++;
+      left = i;
     }
   }
+  const i = left, j = totalLeft - left;
+  const num1Left = i === 0 ? Number.MIN_VALUE : nums1[i - 1];
+  const num1Right = i === nums1Len ? Number.MAX_VALUE : nums1[i];
+  const num2Left = j === 0 ? Number.MIN_VALUE : nums2[j - 1];
+  const num2Right = j === nums2Len ? Number.MAX_VALUE : nums2[j];
+  const leftMax = Math.max(num1Left, num2Left);
+  const rightMin = Math.min(num1Right, num2Right);
+  return (nums1Len + nums2Len) % 2 === 1 ? leftMax : (leftMax + rightMin) / 2;
 
-  if (Math.ceil(mid) !== mid) {
-    return result[Math.floor(mid)];
-  }
-  return (result[mid] + (result[mid - 1] || 0)) / 2;
 };
-console.log(findMedianSortedArrays([1, 3], [2])); // 2
-console.log(findMedianSortedArrays([1, 2], [3, 4])); // 2.5
-console.log(findMedianSortedArrays([0, 0], [0, 0]));// 0
-console.log(findMedianSortedArrays([], [1]));// 1
-console.log(findMedianSortedArrays([2], []));// 2
+// console.log(findMedianSortedArrays([1, 3], [2])); // 2
+// console.log(findMedianSortedArrays([1, 2], [3, 4])); // 2.5
+// console.log(findMedianSortedArrays([0, 0], [0, 0]));// 0
+// console.log(findMedianSortedArrays([], [1]));// 1
+// console.log(findMedianSortedArrays([2], []));// 2
+console.log(findMedianSortedArrays([1, 2, 3, 4, 6, 7, 8], []));// 4
+console.log(findMedianSortedArrays([1, 2, 8, 9, 10], [3, 4, 5, 6, 7, 8, 10, 11, 12, 13]));// 4
