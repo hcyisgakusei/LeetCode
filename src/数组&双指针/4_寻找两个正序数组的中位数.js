bdb0bfb69e705fe7b3890d3dfda9af2c6d2e71b5
chaya.huang <chaya.huang@klook.com>
2021-12-29 16:53:29 +0800

    feat: 寻找两个正序数组的中位数

--- src/数组&双指针/4_寻找两个正序数组的中位数.js
+++ src/数组&双指针/4_寻找两个正序数组的中位数.js
@@ -34,37 +34,37 @@
  * @return {number}
  */
 const findMedianSortedArrays = function (nums1, nums2) {
-  // 找出分割线，使得左边的所有元素 <= 右边的所有元素
+  // 找出分割线，使得左边的所有元素 <= 右边的所有元素 ，分割线在元素个数少的数组中确认
   // 分割线位置：m + n 为偶数 --> left size = right size； m + n 为奇数 -->  left size - 1 = right size(左边多一个元素)
   // 分割线条件：由于两个都是有序数组，在同一个数组内，左边的元素肯定小于等于左边的元素。在不同的数组之间，需要满足交叉满足左边小于等于右边！
-  if (nums2.length > nums1.length) {
-    [nums1, nums2] = [nums2, nums1];
+  if (nums1.length > nums2.length) {
+    [nums1, nums2] = [nums2, nums1]  // 分割线在元素个数少的数组中确认，nums1为个数少的数组
   }
-  const nums1Len = nums1.length, nums2Len = nums2.length;
-  const totalLeft = Math.ceil((nums1Len + nums2Len) / 2);
-  let left = 0, right = nums1Len;
+  const m = nums1.length, n = nums2.length, totalLeft = Math.ceil((m + n) / 2); // totalLeft : 左边元素和
+  let left = 0, right = m; // 在 [0,m]中找出分割线
   while (left < right) {
     let i = left + Math.ceil((right - left) / 2), j = totalLeft - i;
     if (nums1[i - 1] > nums2[j]) {
-      right = i - 1;
+      right = i - 1;  // 下一轮搜索区间[left,i-1]
     } else {
-      left = i;
+      left = i; // 下一轮搜索区间[i,right]
     }
   }
-  const i = left, j = totalLeft - left;
-  const num1Left = i === 0 ? Number.MIN_VALUE : nums1[i - 1];
-  const num1Right = i === nums1Len ? Number.MAX_VALUE : nums1[i];
-  const num2Left = j === 0 ? Number.MIN_VALUE : nums2[j - 1];
-  const num2Right = j === nums2Len ? Number.MAX_VALUE : nums2[j];
-  const leftMax = Math.max(num1Left, num2Left);
-  const rightMin = Math.min(num1Right, num2Right);
-  return (nums1Len + nums2Len) % 2 === 1 ? leftMax : (leftMax + rightMin) / 2;
-
+  let i = left, j = totalLeft - left;
+  const num1Left = i === 0 ? Number.NEGATIVE_INFINITY : nums1[i - 1]; // Number.NEGATIVE_INFINITY -> 负无穷大
+  const num1Right = i === m ? Number.POSITIVE_INFINITY : nums1[i]; // Number.POSITIVE_INFINITY -> 正无穷大
+  const num2Left = j === 0 ? Number.NEGATIVE_INFINITY : nums2[j - 1];
+  const num2Right = j === n ? Number.POSITIVE_INFINITY : nums2[j];
+  const leftMax = Math.max(num1Left, num2Left); // 分割线左边的最大值
+  const rightMin = Math.min(num1Right, num2Right); // 分割线右边的最小值
+  return (m + n) % 2 === 1 ? leftMax : (leftMax + rightMin) / 2;
 };
-// console.log(findMedianSortedArrays([1, 3], [2])); // 2
-// console.log(findMedianSortedArrays([1, 2], [3, 4])); // 2.5
-// console.log(findMedianSortedArrays([0, 0], [0, 0]));// 0
-// console.log(findMedianSortedArrays([], [1]));// 1
-// console.log(findMedianSortedArrays([2], []));// 2
-console.log(findMedianSortedArrays([1, 2, 3, 4, 6, 7, 8], []));// 4
+
+console.log(findMedianSortedArrays([1, 3], [2])); // 2
+console.log(findMedianSortedArrays([1, 2], [3, 4])); // 2.5
+console.log(findMedianSortedArrays([0, 0], [0, 0]));// 0
+console.log(findMedianSortedArrays([], [1]));// 1
+console.log(findMedianSortedArrays([2], []));// 2
+console.log(findMedianSortedArrays([ 2, 3, 4, 6, 7, 8], [1]));// 4
 console.log(findMedianSortedArrays([1, 2, 8, 9, 10], [3, 4, 5, 6, 7, 8, 10, 11, 12, 13]));// 4
+console.log(findMedianSortedArrays([3], [-2, -1]));
